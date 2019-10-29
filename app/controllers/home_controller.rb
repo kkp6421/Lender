e238f207976dd33b07792e626223fcca1227a345
Tomohiro <zealot6421@gmail.com>
2019-10-29 22:41:49 +0900

    Add pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
-  PER = 1
+
   def index
     unless owner_signed_in?
       redirect_to new_owner_session_path
     end
 
-    @books = current_owner.books.paginate(page: params[:page], per_page: 8) if owner_signed_in?
+    @books = current_owner.books.paginate(page: params[:page], per_page: 1) if owner_signed_in?
   end
 
   def about
