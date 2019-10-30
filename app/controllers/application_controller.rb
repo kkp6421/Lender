243fdc2ad7516a8986da0dcaeb0daf1497ecc24f
Tomohiro <zealot6421@gmail.com>
2019-10-30 17:27:27 +0900

    error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+  rescue_from Exception, with: :error500
+
+  def error404
+    render 'error/error404', status: 404, formats: [:html]
+  end
+
+  def error500
+    render 'error/error500', status: 500, formats: [:html]
+  end
 end
