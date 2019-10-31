a13c9deff6e226bf4fba6bc1863a403837fdf065
Tomohiro <zealot6421@gmail.com>
2019-10-31 19:15:47 +0900

    Repair

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,5 +3,5 @@ class Owner < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 end
